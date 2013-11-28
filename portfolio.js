17a50d54e2f4fc50315a9093ae4b1bf424bcbf70
Kyungmin Kim <kmin905@gmail.com>
2013-11-27 22:21:08 -0800

    Add descriptions and links

--- portfolio.js
+++ portfolio.js
@@ -124,50 +124,47 @@ if (Meteor.isServer) {
         {
           title: "Passwordlet",
           url: "http://www.passwordlet.com",
-          img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
-          tags: ["Ruby on Rails", "Backbone", "Animation"],
+          img: "images/passwordlet.png",
+          desc: "Final project at App Academy for 2 weeks. Passwordlet securely stores user credentials and automatically logs in users with cookies.",
+          github: "https://github.com/kyungmin/passwordlet",
+          tags: ["Ruby on Rails", "Postgress", "Backbone", "JavaScript"],
           date: "Nov 13, 2013"
         },
         {
-          title: "99 Cats",
-          url: "http://www.passwordlet.com",
+          title: "99Cats",
+          url: "https://github.com/kyungmin/NinetyNineCats-v2",
           img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
-          tags: ["JavaScript"],
-          date: "Nov 13, 2013"
-        },
-        {
-          title: "Asteroids",
-          url: "http://www.passwordlet.com",
-          img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
-          tags: ["Sketch"],
+          desc: "Full Rails application with multiple associations. 99Cats lets users make reservations on cat rentals.",
+          github: "https://github.com/kyungmin/NinetyNineCats-v2",
+          tags: ["Ruby on Rails"],
           date: "Nov 13, 2013"
         },
         {
           title: "Snake",
-          url: "http://www.passwordlet.com",
-          img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
+          url: "http://goo.gl/oCcvkc",
+          img: "images/snake.png",
+          desc: "Object-oriented JavaScript game with HTML and CSS.",
+          github: "https://github.com/kyungmin/snake",
           tags: ["JavaScript"],
-          date: "Nov 13, 2013"
+          date: "Oct 24, 2013"
+        },
+        {
+          title: "Asteroids",
+          url: "http://goo.gl/WJwyDI",
+          img: "images/asteroids.png",
+          desc: "Object-oriented JavaScript game rendered on HTML5 canvas.",
+          github: "https://github.com/kyungmin/asteroids",
+          tags: ["JavaScript", "Canvas"],
+          date: "Oct 23, 2013"
         },
         {
           title: "Command Line Chess",
           url: "http://www.passwordlet.com",
-          img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
+          img: "images/chess.jpg",
+          desc: "Command line chess program in Ruby.",
+          github: "https://github.com/kyungmin/chess",
           tags: ["Ruby"],
-          date: "Nov 13, 2013"
-        },
-        {
-          title: "Ruby Idioms",
-          url: "http://www.passwordlet.com",
-          img: "http://placekitten.com/300/300",
-          desc: "Final project at App Academy.",
-          tags: ["Ruby on Rails"],
-          date: "Nov 13, 2013"
+          date: "Sep 24, 2013"
         }
       ];
 
